33b1ae79c5ef2db14f3cc13ace4f005d8897da97
biphobe <hi@bipho.be>
2020-08-02 15:59:35 +0200

    Rename linkStations to stationsWithPower for clarity

--- index.ts
+++ index.ts
@@ -6,8 +6,8 @@ const BASE_STATIONS: Station[] = [[0, 0, 10],[20, 20, 5],[10, 0, 12]];
 const devices: Device[] = [[0, 0], [100, 100], [15, 10], [18, 18]];
 
 devices.forEach((device) => {
-  const linkStations: StationWithPower[] = BASE_STATIONS.map(createStationWithPower(device));
-  const mostPowerfulStation = getMostPowerfulStation(linkStations);
+  const stationsWithPower: StationWithPower[] = BASE_STATIONS.map(createStationWithPower(device));
+  const mostPowerfulStation = getMostPowerfulStation(stationsWithPower);
 
   if (mostPowerfulStation.power > 0) {
     console.log(`Best link station for point ${device[0]},${device[1]} is ${mostPowerfulStation.station[0]},${mostPowerfulStation.station[1]} with power ${mostPowerfulStation.power}`);
